cd3e8fd1e52f929799f6a61a9c73fa5f5c902ce2
João Távora <joaotavora@gmail.com>
2015-07-18 21:33:40 +0100

    Lot's of new stuff. Seems to be working more or less OK.
    * livereload.el (livereload--debug, livereload--event)
    (livereload-shutdown-server, livereload-start-server)
    (livereload-mode, livereload--calculated-targets)
    (livereload-potential-targets, livereload-notify-hook)
    (livereload--notify-maybe, livereload-notify)
    (livereload-default-potential-targets): Lots of new stuff.

--- livereload.el
+++ livereload.el
@@ -36,6 +36,9 @@
 (defun livereload--message (control &rest args)
   (message "[livereload] %s" (apply #'format control args)))
 
+(defun livereload--debug (control &rest args)
+  (message "[livereload, debug] %s" (apply #'format control args)))
+
 (defun livereload--opened (connection)
   (livereload--message "%s being told hello!" connection)
   (websocket-send-text (process-get connection :websocket)
@@ -71,11 +74,15 @@
                (string-match "connection-check-1" preferred))
       (livereload--message "%s connection-check left to linger!" connection message)
       (process-put connection 'livereload--connection-check message)
+      ;; (livereload--message "%s connection-check killed!" connection message)
       ;; (livereload-close connection)
       )))
 
 (cl-defmethod livereload--event ((_command (eql :info)) message connection)
-  (livereload--message "%s sends info: %s!" connection message))
+  (livereload--message "%s sends info: %s!" connection message)
+  (let ((url (alist-get 'url message)))
+    (process-put connection 'livereload--url url)
+    (livereload--debug "%s registering url %s" connection url)))
 
 (defun livereload--prompt-for-connection ()
   (unless livereload--connections
@@ -136,12 +143,17 @@
       (livereload--unjack-websocket process incoming)
     (websocket-server-filter process incoming)))
 
-(cl-defun livereload (&optional (listen-port 35729))
+(defun livereload-shutdown-server ()
+  (interactive)
+  (delete-process livereload--server)
+  (livereload--message "Server shutdown"))
+
+(cl-defun livereload-start-server (&optional (listen-port 35729))
   (interactive)
   (when (and livereload--server
              (process-live-p livereload--server))
     (livereload--message "deleting process %s first" livereload--server)
-    (delete-process livereload--server))
+    (livereload-shutdown-server))
   (setq livereload--server
         ;; we can't use `websocket-server' directly, since we want
         ;; to use a slightly different filter function that serves
@@ -164,7 +176,154 @@
                                   (livereload--closed (websocket-conn ws)))
                       :on-error (lambda (ws &rest args)
                                   (apply #'livereload--handle-websocket-error (websocket-conn ws) args)))
-         :service listen-port)))
+         :service listen-port
+         :sentinel (lambda (process change)
+                     (livereload--message "%s changed state to %s. Killing." process change)
+                     (set-process-sentinel process nil)
+                     (delete-process process))))
+  (livereload--message "%s listening on %s" livereload--server listen-port))
+
+(define-minor-mode livereload-mode
+  "Toggle file change notification service LiveReload."
+  :global t :group 'convenience
+  (cond (livereload-mode
+         (livereload-start-server)
+         (add-hook 'after-save-hook 'livereload--notify-maybe 'append))
+        (t
+         (livereload-shutdown-server)
+         (mapc #'livereload-close livereload--connections)
+         (remove-hook 'after-save-hook 'livereload--notify-maybe))))
+
+
+;;; Event stuff
+;;;
+
+(defvar livereload--calculated-targets '())
+
+(defvar livereload-potential-targets
+  'livereload-default-potential-targets
+  "Identifies target URLs based on visited URL and current buffer.
+
+Can be a list of strings, the symbol t, or a symbol denoting a
+function of one argument producing one of the two preceding
+types.
+
+A list of strings identifies target URLs that livereload clients
+connected to Emacs are told to reload. If it is empty (or nil)
+saving the buffer will never cause clients to be notified.
+
+If a function, the argument passed to the function is the URL of
+the webpage that a livereload client is visiting.
+
+If t, it is up to any functions in `livereload-notify-hook',
+which see, to compute and perform any notifications.
+
+This variable is most likely useful if set buffer-locally.
+
+The default value is the function
+`livereload-default-potential-targets' which considers the
+visited URL argument as well as the name and type of the buffer
+being saved to disk, returning target URLs that it believes need
+updating based on a simple heuristic. It is perhaps only suited
+for static websites of plain HTML and CSS files.
+
+More sophisticated functions may better guess the target URLs
+that need to be reloaded. Such a function might, for example,
+scan the buffer's contents to discover these targets, or even
+request via HTTP the URL being visited to scan the resulting
+HTML code.
+
+Another, simpler alternative, might be to manually set this
+variable file-locally to a list of strings.  See Info node
+`Specifying File Variables'.
+
+Finally, setting this variable to t is useful if the target URLs
+cannot be discovered a priori. They may, for example, be some
+preprocessing to be done on the saved file by the functions in
+`livereload-notify-hook' and only from the results of that
+processing can the target URLs be revealed.")
+
+(defvar livereload-notify-hook nil
+  "Hooks to run before notifying livereload clients.
+
+Each hook is a unary function that returns a boolean. The single
+argument passed to the function is a list of visited URL's for
+which `livereload-potentitial-targets' identified a need of
+reloading.
+
+If the hook is empty, or every function returns nil,
+`livereload-notify' is automatically called at the end with no
+arguments, meaning that connected clients are told to reload any
+targets previously identified via `livereload-potential-targets'.
+
+A function placed in this hook that returns non-nil prevents the
+rest of hook from running as well as this default behaviour.
+
+Such a function would normally be responsible, unless it wants to
+abort the notification process, for eventually calling
+`livereload-notify' itself, regardless of whether it chooses to
+call `livereload-notify' immediately, or somehow schedule a call
+to it in the future.")
+
+(defun livereload--notify-maybe ()
+  (let* ((livereload--calculated-targets
+          (cl-loop for conn in livereload--connections
+                   for url = (process-get conn 'livereload--url)
+                   for targets = (and url
+                                      (if (and (not (eq t livereload-potential-targets))
+                                               (symbolp livereload-potential-targets))
+                                          (funcall livereload-potential-targets url)
+                                        livereload-potential-targets))
+                   when targets
+                   collect (list url conn targets))))
+    (cond (livereload--calculated-targets
+           (let ((hookage (run-hook-with-args-until-success
+                           'livereload-notify-hook
+                           (mapcar
+                            #'car livereload--calculated-targets))))
+             (unless hookage
+               (livereload-notify))))
+          (t
+           (livereload--debug "No one to notify for %s" (current-buffer))))))
+
+(defun livereload-notify (&optional targets)
+  (cl-loop
+   for (_url conn calculated-targets) in livereload--calculated-targets
+   do (cl-loop for target in (or targets
+                                 (unless (eq calculated-targets t)
+                                   calculated-targets))
+               do (websocket-send-text
+                   (process-get conn :websocket)
+                   (json-encode `((command . :reload)
+                                  (path . ,target)
+                                  (liveCSS . t))))))
+  ;; prevent accidental overcalling of `livereload-notify'
+  (setq livereload--calculated-targets nil))
+
+(defun livereload-default-potential-targets (url)
+  (when (and (string-match "^\\(https?://\\(?:localhost\\|127.0.0.1\\)\\(?::[[:digit:]]+\\)?\\)\\(.*\\)"
+                           url)
+             buffer-file-name
+             (save-match-data
+               (string-match "\\(css\\|\\js\\|html?\\)" (file-name-extension buffer-file-name))))
+    (let* ((_address (match-string 1 url))
+           (path (match-string 2 url))
+           (project-dir (and buffer-file-name
+                             (locate-dominating-file buffer-file-name ".git")))
+           (relative (and project-dir
+                          (substring buffer-file-name
+                                     (cl-mismatch project-dir buffer-file-name)))))
+      (cond ((and relative
+                  (string-match "\\(css\\|\\js\\)" (file-name-extension relative)))
+             (list relative))
+            ((and relative
+                  (string-match "index.html?" (file-name-nondirectory relative)))
+             (let ((relative-path (concat "/"
+                                          (file-name-directory relative))))
+               (when (string= (regexp-quote relative-path) path)
+                 (list relative-path))))))))
+
+
 
 (provide 'livereload)
 ;;; livereload.el ends here
